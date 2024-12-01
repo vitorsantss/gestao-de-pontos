4ed8fd753f2b1ea1589b0667509c4b385ed9de80
Vitor Santos <santoss.coding@gmail.com>
2024-12-01 13:33:01 -0300

    chore: initialize Spring Boot project with basic setup

---
+++ src/main/java/com/sants/gestaodeponto/GestaodepontoApplication.java
@@ -0,0 +1,13 @@
+package com.sants.gestaodeponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaodepontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaodepontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sants/gestaodeponto/GestaodepontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sants.gestaodeponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaodepontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
