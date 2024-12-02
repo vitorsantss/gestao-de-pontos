fc826f4698307b5c7937fb356e83ede3b9645bad
Vitor Santos <santoss.coding@gmail.com>
2024-12-01 21:06:43 -0300

    feat(repository): add repository for work log

--- src/main/java/com/sants/gestaodeponto/repositories/WorkLogRepository.java
+++ src/main/java/com/sants/gestaodeponto/repositories/WorkLogRepository.java
@@ -3,5 +3,14 @@ package com.sants.gestaodeponto.repositories;
 import com.sants.gestaodeponto.domain.worklog.WorkLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface WorkLogRepository extends JpaRepository<WorkLog, String> {
+
+    List<WorkLog> findByUserIdAndTimestampAfter(String userId, LocalDateTime timestamp);
+
+    List<WorkLog> findByUserIdOrderByTimestampDesc(String userId);
+
+    List<WorkLog> findByUserId(String userId);
 }
